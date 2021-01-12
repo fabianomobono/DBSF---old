f8aa7810f30ca87118b68bead7e78b72b4f0ba1a
fabianomobono <fabian.omobono@gmail.com>
2021-01-12 13:08:10 -0500

    testing;

--- DBSF/settings.py
+++ DBSF/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 AUTH_USER_MODEL = 'social.User'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['https://fierce-oasis-17820.herokuapp.com/*']
 
 # Application definition
