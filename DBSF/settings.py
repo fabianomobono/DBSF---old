7ca91fecff99f3e1acb82a8e3f6e5853eba7d25a
fabianomobono <fabian.omobono@gmail.com>
2021-01-12 12:28:48 -0500

    testing socket

--- DBSF/settings.py
+++ DBSF/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 AUTH_USER_MODEL = 'social.User'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 SECURE_SSL_REDIRECT = True
