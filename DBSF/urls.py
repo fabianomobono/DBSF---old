018805e27f00f1ad2f3e1f4dea1cc0dc787d7763
fabianomobono <fabian.omobono@gmail.com>
2021-01-13 13:01:10 -0500

    debug False test

--- DBSF/urls.py
+++ DBSF/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('', include('social.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
+    
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
