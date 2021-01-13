ece66241441914b5e1f42b30b755b95a4a4036e0
fabianomobono <fabian.omobono@gmail.com>
2021-01-13 13:09:22 -0500

    debug

--- DBSF/urls.py
+++ DBSF/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+if  not settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
