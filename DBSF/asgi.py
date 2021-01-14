10564a4f6cb652647a9587c6b20adc29e6b0c74a
fabianomobono <fabian.omobono@gmail.com>
2021-01-14 15:02:45 -0500

    sockets still not working

--- DBSF/asgi.py
+++ DBSF/asgi.py
@@ -7,13 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DBSF.settings")
 
 import django
 django.setup()
 
 from django.core.management import call_command
-import os
+
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
