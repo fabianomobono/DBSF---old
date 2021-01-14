bc548bdb65508b69048d12483647e3c09999c363
fabianomobono <fabian.omobono@gmail.com>
2021-01-14 14:55:53 -0500

    broke

--- DBSF/asgi.py
+++ DBSF/asgi.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DBSF.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 import django
 django.setup()
