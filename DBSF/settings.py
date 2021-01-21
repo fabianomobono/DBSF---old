6a065ac1cb8517cee333f85df3ce6f95ae8a65c1
fabianomobono <fabian.omobono@gmail.com>
2021-01-20 23:16:29 -0500

    added heroku tips4

--- DBSF/settings.py
+++ DBSF/settings.py
@@ -145,4 +145,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
 MEDIA_URL= "/media/"
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'social/static'),)
