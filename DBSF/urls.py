2e1a6083247dafa1cc9e3e7d058597a31b04dcc7
fabianomobono <fabian.omobono@gmail.com>
2021-04-09 21:43:29 -0400

    testing fake login

--- DBSF/urls.py
+++ DBSF/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
@@ -24,7 +25,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('mobileapi.urls')),
-    
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
--- mobileapi/urls.py
+++ mobileapi/urls.py
@@ -5,6 +5,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
   path('', views.api, name='api_view'),
   path('users', views.users, name='user_api'),
-  path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+  
   path('login', views.login, name='fake_login')
 ]
