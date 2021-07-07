3cd51f0d1d82e183b925767977fb3acf3929214f
fabianomobono <fabian.omobono@gmail.com>
2021-07-07 06:11:03 -0400

    added SOCIAL_AUTH_USER_MODEL

--- DBSF/settings.py
+++ DBSF/settings.py
@@ -25,6 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 AUTH_USER_MODEL = 'social.User'
+SOCIAL_AUTH_USER_MODEL = 'social.User'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = ['desolate-lowlands-74512.herokuapp.com', 'dbsf.herokuapp.com', 'localhost', '127.0.0.1', 'testserver']
